cd1a96621b89c42ae06ef95f433f6a690bf4c4c5
Michael Newman <coloradohokie@gmail.com>
2019-12-19 20:16:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,6 +10,7 @@ def map_to_negativize(array)
 end
 
 def map_to_no_change(array)
+  i=0
   new_array = []
   while i < array.length do
     new_array.push (array[i])
@@ -30,7 +31,7 @@ end
 
 def map_to_square(array)
   i=0
-  new_array
+  new_array = []
   while i < array.length do
     new_array.push(array[i] ** 2)
     i += 1
